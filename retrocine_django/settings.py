74c7bd245671d05b787323f13acfb6d3135c3d77
EricBarzman <ericbarzman@gmail.com>
2024-07-23 16:41:51 +0200

    vercel deploy

--- retrocine_django/settings.py
+++ retrocine_django/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,7 +13,7 @@ SECRET_KEY = 'django-insecure-8+7i3*474ub1-*o)@6g357db5ht26e5j36cfstbh19o5+mg4!w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 WEBSITE_URL = 'http://127.0.0.1:8000/'
 
