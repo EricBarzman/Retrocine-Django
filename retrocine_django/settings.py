8065f8b26c48a03d7e7d5dee287d4a8a64d0ee45
EricBarzman <ericbarzman@gmail.com>
2024-08-07 18:43:05 +0200

    bug movie year date integer

--- retrocine_django/settings.py
+++ retrocine_django/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'retrocine_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'retrocine',
-        'USER': 'root',
-        'PASSWORD': 'strato68',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'retrocine',
+#         'USER': 'root',
+#         'PASSWORD': 'strato68',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
