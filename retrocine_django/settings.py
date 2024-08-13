2dd955e8e56f2633f0bf993a60ed241068f59024
EricBarzman <ericbarzman@gmail.com>
2024-08-13 13:53:54 +0200

    render

--- retrocine_django/settings.py
+++ retrocine_django/settings.py
@@ -93,10 +93,7 @@ WSGI_APPLICATION = 'retrocine_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
 }
 
 # DATABASES = {
